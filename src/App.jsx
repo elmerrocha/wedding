0e5b21a88f6384acd15fa720f80b9445352362cd
Elmer Rocha <elmer.rocha.j@gmail.com>
2025-07-30 14:52:58 +1000

    Envelope intro and music

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Blessing from "./components/Blessing";
 import Ceremony from "./components/Ceremony";
 import Countdown from "./components/Countdown";
@@ -6,27 +7,52 @@ import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
 import Gifts from './components/Gifts';
 import Header from "./components/Header";
+import Intro from "./components/Intro";
 import Reception from "./components/Reception";
 import Rsvp from "./components/Rsvp";
 import Tips from "./components/Tips";
 
 
 export default function App() {
+  const [opened, setOpened] = useState(false);
   return (
-    <div className="bg-cremarustico text-verdeOscuro min-h-screen">
-      <Header />
-      <Blessing />
-      <main className="space-y-10 p-4 max-w-4xl mx-auto">
-        <Countdown />
-        <Gallery />
-        <Ceremony />
-        <Reception />
-        <DressCode />
-        <Rsvp />
-        <Tips />
-        <Gifts />
-      </main>
-      <Footer />
-    </div>
+      <>
+        {!opened ? (
+          <Intro onOpen={() => setOpened(true)} />
+        ) : (
+          <div className="bg-cremarustico text-verdeOscuro min-h-screen">
+            <Header />
+            <Blessing />
+            <main className="space-y-10 p-4 max-w-4xl mx-auto">
+              <Countdown />
+              <Gallery />
+              <Ceremony />
+              <Reception />
+              <DressCode />
+              <Rsvp />
+              <Tips />
+              <Gifts />
+            </main>
+            <Footer />
+          </div>
+        )}
+      </>
   );
+  // return (
+  //   <div className="bg-cremarustico text-verdeOscuro min-h-screen">
+  //     <Header />
+  //     <Blessing />
+  //     <main className="space-y-10 p-4 max-w-4xl mx-auto">
+  //       <Countdown />
+  //       <Gallery />
+  //       <Ceremony />
+  //       <Reception />
+  //       <DressCode />
+  //       <Rsvp />
+  //       <Tips />
+  //       <Gifts />
+  //     </main>
+  //     <Footer />
+  //   </div>
+  // );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+import Lottie from 'lottie-react';
+import envelopeAnim from '../assets/intro.json';
+import useSound from 'use-sound';
+import weddingSong from '../assets/wedding-song.mp3';
+
+const Intro = ({ onOpen }) => {
+    const [play] = useSound(weddingSong, { volume: 0.5 });
+    const lottieRef = useRef();
+    const [hasClicked, setHasClicked] = useState(false);
+
+    const handleOpen = () => {
+        if (hasClicked) return;
+        setHasClicked(true);
+        play();
+
+        if (lottieRef.current) {
+        lottieRef.current.play();
+        }
+
+        setTimeout(() => {
+        onOpen();
+        }, 2800);
+    };
+
+    return (
+        <div className="relative h-screen w-full bg-cremarustico flex justify-center items-center overflow-hidden">
+
+        <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2.0, delay: 0.5, type: 'spring' }}
+            onClick={handleOpen}
+            className="absolute bottom-40 md:bottom-40 z-10 text-3xl md:text-3xl font-vibes font-bold text-verdeOscuro cursor-pointer select-none"
+        >
+            Pulsa para abrir
+        </motion.p>
+
+        <Lottie
+            lottieRef={lottieRef}
+            animationData={envelopeAnim}
+            loop
+            onClick={handleOpen}
+            autoplay={false}
+            className="w-full h-full object-cover"
+        />
+        </div>
+    );
+};
+
+export default Intro;
