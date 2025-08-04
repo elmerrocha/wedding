909d90c8f3a048cc802d4eea8c07809cacb93b64
Elmer Rocha <elmer.rocha.j@gmail.com>
2025-08-05 08:50:00 +1000

    Reception hour removed

--- src/components/Reception.jsx
+++ src/components/Reception.jsx
@@ -13,10 +13,9 @@ export default function Reception() {
         >
         <Lottie animationData={celebrationAnim} loop className="w-52 h-52 mx-auto" />
         <h2 className="text-6xl font-vibes text-verdeOscuro my-4">Recepción</h2>
-        <p className="text-4xl font-vibes">
+        <p className="text-4xl font-vibes mb-3">
             Hacienda El Ensueño<br />Ruitoque Alto
         </p>
-        <p className="text-3xl font-vibes my-3">5:00 p.m.</p>
         <iframe
             className="mt-6 w-full h-72 rounded-3xl shadow-2xl mb-16"
             src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3959.7174267177547!2d-73.0928383250024!3d7.042456692959644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMDInMzIuOCJOIDczwrAwNScyNS4wIlc!5e0!3m2!1sen!2sau!4v1753546809544!5m2!1sen!2sau"
